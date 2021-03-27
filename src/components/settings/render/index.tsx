658fcf9e5e5a0dd15ac96a28ea9ce6f2d5371661
Paul Kretschel <paul.kretschel@icloud.com>
2021-03-27 14:13:32 +0100

    Add render mode setting

--- src/components/settings/render/index.tsx
+++ src/components/settings/render/index.tsx
@@ -1,12 +1,32 @@
 import React from "react";
 
 import { Heading } from "..";
+import { AppMode, useStore } from "../../../state";
+import { RenderMode } from "../../../state/rendering";
 import { DownloadRender } from "./download";
 
 export const RenderSettings: React.FC = () => {
+  const [renderMode, setRenderMode] = useStore((state) => [
+    state.renderMode,
+    state.setRenderMode,
+  ]);
+
   return (
     <>
       <Heading>Render</Heading>
+      <div className="mb-4">
+        <span className="mr-3">Mode:</span>
+        <select
+          className="cursor-pointer bg-transparent border-2 border-gray-700 text-white"
+          onChange={({ target }) =>
+            setRenderMode(parseInt(target.value) as RenderMode)
+          }
+          value={renderMode}
+        >
+          <option value={RenderMode.Download}>Download</option>
+          <option value={RenderMode.Server}>Server</option>
+        </select>
+      </div>
       <DownloadRender />
     </>
   );
